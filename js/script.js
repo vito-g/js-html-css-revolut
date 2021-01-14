a577cccd0d74edcff509655f90e6e7148cf372df
Vito Gentile <gentilevito1076@gmail.com>
2021-01-14 13:44:36 +0100

    TRACCIA Inserita  e resa commento

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+// TRACCIA:
+// Descrizione:
+// Sviluppiamo il fratello maggiore del dropdown visto oggi in classe.
+// Dobbiamo copiare la nav da questo sito https://www.revolut.com/en-IT/ e sviluppare il suo dropdown.
+// Mi raccomando: solo la prima parte, solo la navbar.
+// In questo caso quindi lavoreremo anche lato HTML. Sviluppiamo il nostro html + css e il nostro codice jquery per far comparire o sparire il dropdown menu.
+// Consigli del giorno:
+// Proseguiamo per step e completiamoli prima ancora di passare al problema successivo (e soluzione).
+// Lavoriamo lato HTML tenendo però sempre ben presente su quali aspetti dovrà poi lavorare il nostro jquery.
+// quali classi?
+// cosa attiverà, cosa disattiverà?
+// quando la funzione jquery dovrà essere scatenata? Dopo quale evento?
+// come dovrà essere la struttura html per poter poi sfruttare (come abbiamo visto in classe) il .find()?
+
+
 $(document).ready(function() {
 
 
